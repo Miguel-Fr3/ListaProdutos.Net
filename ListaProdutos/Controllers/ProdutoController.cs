ed5adef84d292ebbaea3e781c2f4ca022ee24436
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-05-02 22:45:12 -0300

    Initial

---
+++ ListaProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,86 @@
+﻿using ListaProdutos.Data;
+using ListaProdutos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaProdutos.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProdutoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View(_context.Produto.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Produto newProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Produto.Add(newProduto);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newProduto);
+        }
+
+        public IActionResult Edit(int ID)
+        {
+            var produto = _context.Produto.Find(ID);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(produto).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+        public IActionResult Delete(int ID)
+        {
+            var produto = _context.Produto.Find(ID);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int ID)
+        {
+            var produto = _context.Produto.Find(ID);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produto.Remove(produto);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ListaProdutos/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ListaProdutos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaProdutos.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
